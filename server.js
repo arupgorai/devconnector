1e16623aad35465b6f040305233b577a2d328582
Arup Gorai <arupgorai.js@gmail.com>
2020-11-28 15:53:03 +0530

    routes added

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.get('/', (req, res) => {
   res.send('api running');
 });
 
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
