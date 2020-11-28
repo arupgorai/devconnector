d08760ade82caa93636ab535313c550cc179d206
Arup Gorai <arupgorai.js@gmail.com>
2020-11-28 18:38:13 +0530

    login api

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const config = require('config');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const {body, validationResult} = require('express-validator');
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
 
@@ -12,6 +16,63 @@ router.get('/', auth, async (req, res) => {
     res.json(user)
   } catch (err) {
     console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route    POST api/auth
+// desc      Authenticate User an get token
+// @access   Public
+router.post('/', [
+  body('email', 'Please include a valid email').isEmail(),
+  body('password', 'Password is required').exists()
+], async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()})
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    // see if user exist
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        errors: [{ msg: 'Invalid Credentials' }]
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        errors: [{ msg: 'Invalid Credentials' }]
+      });
+    }
+
+    // return jsonwebtoken
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
+
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      {expiresIn: 360000},
+      (err, token) => {
+        if (err) {
+          throw err;
+        }
+        res.json({ token })
+      }
+    )
+  } catch (er) {
+    console.error(er.message);
     res.status(500).send('Server Error');
   }
 });
