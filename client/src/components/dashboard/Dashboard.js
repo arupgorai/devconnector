0863f37b427572b57ede2a084196b4a2dbaca6e3
Arup Gorai <arupgorai.js@gmail.com>
2020-12-08 01:21:32 +0530

    a/c delete done

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,13 +2,14 @@ import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
 import { connect } from 'react-redux';
-import { getCurrentProfile } from '../../actions/profile';
+import { deleteAccount, getCurrentProfile } from '../../actions/profile';
 import Spinner from '../layout/Spinner';
 import DashboardActions from './DashboardActions';
 import Experience from './Experience';
 import Education from './Education';
 
 const Dashboard = ({
+  deleteAccount,
   getCurrentProfile,
   auth: {user},
   profile: {profile, loading}
@@ -31,6 +32,13 @@ const Dashboard = ({
             <DashboardActions />
             <Experience experience={profile.experience} />
             <Education education={profile.education} />
+            <div className="my-2">
+              <button className="btn btn-danger" onClick={deleteAccount}>
+                <i className="fas fa-user-minus"></i>
+                {' '}
+                Delete My Account
+              </button>
+            </div>
           </>
         ) : (
           <>
@@ -44,6 +52,7 @@ const Dashboard = ({
 
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
+  deleteAccount: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired
 }
@@ -53,4 +62,4 @@ const mapDispatchToProps = state => ({
   profile: state.profile
 });
 
-export default connect(mapDispatchToProps, { getCurrentProfile })(Dashboard);
+export default connect(mapDispatchToProps, { getCurrentProfile, deleteAccount })(Dashboard);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const {body, validationResult} = require('express-validator');
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 
 // @route    GET api/profile/me
@@ -140,7 +141,8 @@ router.get('/user/:user_id', async (req, res) => {
 // @access   Private
 router.delete('/', auth, async (req, res) => {
   try {
-    // @todo remove posts
+    // remove user posts
+    await Post.deleteMany({ user: req.user.id });
 
     // remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
