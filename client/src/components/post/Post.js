b241a59a4f8c4bf0682603aa5c7bb6d7dcb1c10c
Arup Gorai <arupgorai.js@gmail.com>
2020-12-09 13:01:07 +0530

    comment listed and delete comment functionality added

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import PostItem from '../posts/PostItem';
 import Spinner from '../layout/Spinner';
 import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
 import { getPost } from '../../actions/post';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
@@ -20,6 +21,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
           <Link to='/posts' className="btn">Back to Posts</Link>
           <PostItem post={post} showActions={false} />
           <CommentForm postId={post._id} />
+          <div className="comments">
+            {post.comments.map(comment => (
+              <CommentItem key={comment._id} comment={comment} postId={post._id} />
+            ))}
+          </div>
         </>
       )}
     </>
